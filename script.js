03901224a3eb6ba8319849c8d592faa755bb2a94
subham07-t <82716446+subham07-t@users.noreply.github.com>
2022-08-28 13:56:11 +0530

    added

--- script.js
+++ script.js
@@ -6,28 +6,28 @@ const sec_el = document.getElementById("seconds");
 const newyear = "2 oct 2022";
 
 const newYear = () => {
-    
-    const newYearDate = new Date(newyear);
-    const currentYear = new Date();
+  const newYearDate = new Date(newyear);
+  const currentYear = new Date();
 
-    const Totalseconds  =  (newYearDate - currentYear) / 1000;
-    const days = Math.floor(Totalseconds / (3600*24));
-    const hours = Math.floor(Totalseconds % (3600*24) / 3600);
-    const mins = Math.floor(Totalseconds % 3600 / 60);
-    const seconds = Math.floor(Totalseconds % 60);
+  const Totalseconds = (newYearDate - currentYear) / 1000;
+  const days = Math.floor(Totalseconds / (3600 * 24));
+  const hours = Math.floor((Totalseconds % (3600 * 24)) / 3600);
+  const mins = Math.floor((Totalseconds % 3600) / 60);
+  const seconds = Math.floor(Totalseconds % 60);
 
-    // console.log(days,hour,minutes,sec);
+  // console.log(days,hour,minutes,sec);
 
+  days_el.innerHTML = days;
+  hour_el.innerHTML = timee(hours);
+  min_el.innerHTML = timee(mins);
+  sec_el.innerHTML = timee(seconds);
 
-     days_el.innerHTML = days;
-     hour_el.innerHTML = timee(hours);
-     min_el.innerHTML = timee(mins);
-     sec_el.innerHTML = timee(seconds);
-
-     function timee(time){
-        return time < 10 ? `0${time}` : time
-     }
-}
+  function timee(time) {
+    return time < 10 ? `0${time}` : time;
+  }
+};
 
 newYear();
-setInterval(newYear,1000)
+setInterval(newYear, 1000);
+
+// document.getElementById("vid").play();
